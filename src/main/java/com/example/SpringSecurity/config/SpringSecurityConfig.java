45e1106a90e9e9f55bca31c0dd3011b4c6f63977
Misha <ggvbgg@list.ru>
2023-02-01 21:47:14 +0300

    custom login page and config in SpringSecurityConfig

--- src/main/java/com/example/SpringSecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.SpringSecurity.sevices.PersonDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,13 +20,31 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailService = personDetailService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailService);
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
--- src/main/java/com/example/SpringSecurity/controllers/HelloController.java
+++ src/main/java/com/example/SpringSecurity/controllers/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello () {
