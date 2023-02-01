2743a78aec86d4194c809af1b12a73d19e2a0f09
Misha <ggvbgg@list.ru>
2023-02-01 19:43:03 +0300

    authentication part2

--- src/main/java/com/example/SpringSecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/config/SpringSecurityConfig.java
@@ -1,22 +1,32 @@
 package com.example.SpringSecurity.config;
 
-import com.example.SpringSecurity.security.AuthenticationProviderImpl;
+import com.example.SpringSecurity.sevices.PersonDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthenticationProviderImpl authenticationProvider;
+    private final PersonDetailService personDetailService;
 
     @Autowired
-    public SpringSecurityConfig(AuthenticationProviderImpl authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
+    public SpringSecurityConfig(PersonDetailService personDetailService) {
+        this.personDetailService = personDetailService;
     }
 
+
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+        auth.userDetailsService(personDetailService);
     }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
--- src/main/java/com/example/SpringSecurity/sevices/PersonDetailService.java
+++ src/main/java/com/example/SpringSecurity/sevices/PersonDetailService.java
@@ -4,26 +4,27 @@ import com.example.SpringSecurity.models.Person;
 import com.example.SpringSecurity.repositories.PersonRepository;
 import com.example.SpringSecurity.security.PersonDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
-@Transactional
-public class PersonDetailsService {
+public class PersonDetailService implements UserDetailsService {
+
     private final PersonRepository personRepository;
 
     @Autowired
-    public PersonDetailsService(PersonRepository personRepository) {
+    public PersonDetailService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public PersonDetails findByName (String name){
-        Optional<Person> person = personRepository.findByName(name);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = personRepository.findByName(username);
         if (person.isEmpty()) throw new UsernameNotFoundException("User not found");
         else return new PersonDetails(person.get());
     }
-
 }
