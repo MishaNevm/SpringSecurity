14e4343ae1f0f29beec3e858f61667fcefa5c5f0
Misha <ggvbgg@list.ru>
2023-02-05 22:03:26 +0300

    password encoder

--- src/main/java/com/example/SpringSecurity/sevices/PersonService.java
+++ src/main/java/com/example/SpringSecurity/sevices/PersonService.java
@@ -3,23 +3,24 @@ package com.example.SpringSecurity.sevices;
 import com.example.SpringSecurity.models.Person;
 import com.example.SpringSecurity.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 public class PersonService {
     private final PersonRepository personRepository;
-
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void save (Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         personRepository.save(person);
     }
 
